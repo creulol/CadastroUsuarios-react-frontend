09488e4b17bf6f70105609444b2a97ae7dbc3e12
Luiz Henrique <creulol9@outlook.com>
2020-04-13 23:02:08 -0300

    v0.7 Added a initial page

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Main from '../template/Main'
+
+export default props =>
+    <Main icon="home" title="Home"
+        subtitle="Initial page.">
+        <div className="display-4">Welcome!</div>
+        <hr></hr>
+        <p className="mb-0">User registration system developed in React</p>
+    </Main>
--- src/main/App.jsx
+++ src/main/App.jsx
@@ -5,14 +5,13 @@ import React from 'react'
 
 import Logo from '../components/template/Logo'
 import Nav from '../components/template/Nav'
-import Main from '../components/template/Main'
+import Home from '../components/home/Home'
 import Footer from '../components/template/Footer'
 
 export default props =>
     <div className="app">
         <Logo></Logo>
         <Nav></Nav>
-        <Main icon="home" title="Home"
-            subtitle="Return to the home screen."></Main>
+        <Home></Home>
         <Footer></Footer>
     </div>
