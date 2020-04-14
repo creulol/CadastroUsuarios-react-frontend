760b968c2cd4d0198dde807dc23e59ef260f6e7a
Luiz Henrique <creulol9@outlook.com>
2020-04-13 21:43:49 -0300

    v0.5 Added Simple Footer

--- src/components/template/Footer.jsx
+++ src/components/template/Footer.jsx
@@ -3,5 +3,7 @@ import React from 'react'
 
 export default props =>
     <footer className="footer">
-        Rodapé
+        <span>
+            Desenvolvido com <i className="fa fa-heart text-danger"></i> por <strong><span className="text-primary">L</span>uiz <span className="text-primary">H</span>enrique</strong>
+        </span>
     </footer>
