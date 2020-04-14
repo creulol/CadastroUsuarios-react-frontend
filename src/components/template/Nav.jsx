fa386d13e5b55c8094b921e6e45b8844879bbb4b
Luiz Henrique <creulol9@outlook.com>
2020-04-13 22:05:29 -0300

    v0.6 Added Simple Navigation area

--- src/components/template/Nav.jsx
+++ src/components/template/Nav.jsx
@@ -3,5 +3,13 @@ import React from 'react'
 
 export default props =>
     <aside className="menu-area">
-        Navegacao
+        <nav className="menu">
+            {/* Refatorar para NavItem Component*/}
+            <a href="#/">
+                <i className="fa fa-home"></i> Home
+            </a>
+            <a href="#/users">
+                <i className="fa fa-users"></i> Users
+            </a>
+        </nav>
     </aside>
